2cebe5f65906cf7cfd977bc3ae17debfb80feeef
Adrian Ramirez <adrian.ramirez45679@gmail.com>
2023-09-08 10:48:56 -0700

    Adrian React Application

---
+++ recipe-application/src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Header from "./components/Header"
+import Recipe from "./components/Recipe"
+import Footer from "./components/Recipe"
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Recipe />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ recipe-application/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Footer = () => {
+  return (
+    <>
+      <footer>LEARN Academy | Instruction Team &copy; 2023</footer>
+    </>
+  )
+}
+
+export default Footer
