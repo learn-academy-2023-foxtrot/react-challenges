4168f801a14b9eee89d9c6d9531526d962ae67a6
shaunfitzgarald <shaunfitzgarald@gmail.com>
2023-09-11 14:53:49 -0700

    Worked the challenges

---
+++ state-shaun-anabella/state-shaun-anabella/src/App.js
@@ -0,0 +1,25 @@
+import React, {useState} from "react"
+import "./App.css"
+
+// const App = () => {
+  // const colors = ["white", "red", "orange", "yellow", "green", "blue", "purple", "pink"]
+  // const [currentColorIndex, setCurrentColorIndex] = useState(0)
+  // const switchColor = () => {
+  //   setCurrentColorIndex(currentColorIndex + 1)
+  // }
+//   return (
+//     <>
+//       <h1>Color Switcher</h1>
+//       <div className="color-box">{setCurrentColorIndex}</div>
+//       <p> colors: {colors} </p>
+//       <button onClick={switchColor} style={{ backgroundColor: "white" }}> Change color </button>
+//     </>
+//   )
+// }
+// export default App
+
+const App = () => {
+  const colors = ["white", "red", "orange", "yellow", "green", "blue", "purple", "pink"] = useState (0)
+  <h1> Color Box </h1>
+  <p> Colors: {colors} </p>
+  <button>  </button>
