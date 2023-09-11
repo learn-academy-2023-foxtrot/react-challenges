2ff78a9a9c746f7b0dbd4e432b7fe590210f1a11
shaunfitzgarald <shaunfitzgarald@gmail.com>
2023-09-11 15:53:57 -0700

    made changes to App.js

--- state-shaun-anabella/state-shaun-anabella/src/App.js
+++ state-shaun-anabella/state-shaun-anabella/src/App.js
@@ -20,6 +20,33 @@ import "./App.css"
 
 const App = () => {
   const colors = ["white", "red", "orange", "yellow", "green", "blue", "purple", "pink"] = useState (0)
+  return (
+    <>
   <h1> Color Box </h1>
   <p> Colors: {colors} </p>
-  <button>  </button>
+  < button.color-box {
+    border: 2px solid black;
+    height: 200px;
+    width: 200px > Turn on Color </button>
+
+      <Tracker />
+      <h2> Red: </h2>
+      <Tracker />
+      <h2> Orange: </h2>
+      <Tracker />
+      <h2> Yellow: </h2>
+      <Tracker />
+      <h2> Green: </h2>
+      <Tracker />
+      <h2> Blue: </h2>
+      <Tracker />
+      <h2> Purple: </h2>
+      <Tracker />
+      <h2> Pink: </h2>
+      <Tracker />
+      </>
+    )
+  }
+
+
+export default App
