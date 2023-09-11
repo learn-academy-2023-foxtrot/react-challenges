31d5f1ae61341d781cef87f29f47382ded49ef64
malsherer <mgracesherer@gmail.com>
2023-09-11 13:36:30 -0700

    started challenge

---
+++ state-adrian-mal/src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import SquareBox from "./components/SquareBox1";
+import "./App.css"
+const App = () => {
+  return (
+    <>
+    <SquareBox />
+    </>
+  )
+}
+
+export default App;
---
+++ state-adrian-mal/src/components/SquareBox1.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const SquareBox = () => {
+    return ( 
+        
+        <>
+        <div className="box">
+
+        </div>
+        </>
+    )
+}
+export default SquareBox
