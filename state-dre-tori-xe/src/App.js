880b51c68ba4a2cbafaeb626b184704f1f121304
tori-calkins <tori.calkins2705@gmail.com>
2023-09-11 16:33:04 -0700

    updated version

--- state-dre-tori-xe/src/App.js
+++ state-dre-tori-xe/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import './App.css'
 import ColorBox from './components/ColorBox.js'
-import useGenerateRandomColor from './components/useGenerateRandomColor'
+import './App.css'
 
 const App = () => {
   return (
@@ -9,24 +8,8 @@ const App = () => {
       <h2>Color Box</h2>
       <div className="clicker">
         <ColorBox />
-        <ColorBox />
-        <ColorBox />
-      </div>
-      const { color, generateColor }
-        = useGenerateRandomColor();
-      <div>
-      <button
-                style={{
-                    padding: "40px",
-                    borderRadius: "10px",
-                    backgroundImage:
-                        "linear-gradient(to top, #a8edea 0%, #fed6e3 100%)",
-                    fontSize: "larger",
-                }}
-                onClick={generateColor}
-            >
-                Generate random color
-            </button>
+        {/* <ColorBox /> */}
+        {/* <ColorBox /> */}
       </div>
     </>
   )
