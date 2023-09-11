aa5772fa336424235463f640e2021ccdac2dcaa0
Adrian Ramirez <adrian.ramirez45679@gmail.com>
2023-09-11 15:28:01 -0700

    first few changes

--- state-adrian-mal/src/App.js
+++ state-adrian-mal/src/App.js
@@ -1,10 +1,32 @@
-import React from "react"
+import React, { useState } from "react"
 import SquareBox from "./components/SquareBox1";
 import "./App.css"
 const App = () => {
+  const [boxCount, setBoxCount] = useState(0);
+
+  const addBox = () => {
+    setBoxCount(boxCount + 1);
+  };
+
+  const removeBox = () => {
+    if (boxCount > 0) {
+      setBoxCount(boxCount - 1);
+    }
+  }
+  
+ 
+  //create new box;
+  
+
   return (
     <>
+    <h2>Challenge: Color Box</h2>
+    <button>Add Box</button>
+    <button>Remove Box</button>
+    
     <SquareBox />
+    
+    
     </>
   )
 }
--- state-adrian-mal/src/components/SquareBox1.js
+++ state-adrian-mal/src/components/SquareBox1.js
@@ -1,12 +1,37 @@
-import React from "react"
+import React, { useState }  from "react"
 
 const SquareBox = () => {
-    return ( 
-        
-        <>
-        <div className="box">
+    
+    const colorNames = ['red', 'green','orange,', 'yellow', 'blue', 'pink']
+    const [boxColors, setBoxColors] = useState('white');
+    //const [displayColorName, setDisplayedColor] = useState(0);
 
-        </div>
+    
+    const grabColor = () => {
+        const generateColors = Math.floor(Math.random() * colorNames.length);
+         setBoxColors(colorNames[generateColors]);
+    }
+
+    /*
+    const clickEvent = () => {
+       if (setDisplayedColor === "true") {
+        setBoxColors(grabColor());
+       } else {
+        setBoxColors(grabColor());
+        setDisplayedColor(true)
+       }
+    }
+*/
+    
+
+    return ( 
+        <>
+    
+            <div className="box">
+                <button onClick={grabColor}>Enter</button>
+                {boxColors}
+            </div>
+            
         </>
     )
 }
