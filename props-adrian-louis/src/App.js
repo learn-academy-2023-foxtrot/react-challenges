59ea23cf8ea2b6bda51aa7661fae410002548412
shaunfitzgarald <shaunfitzgarald@gmail.com>
2023-09-13 16:40:21 -0700

    Worked the challenges

--- props-adrian-louis/src/App.js
+++ props-adrian-louis/src/App.js
@@ -2,31 +2,34 @@ import React,{useState} from "react"
 import "./App.css"
 import Dice from "./components/Dice.js"
 import Rolls from "./components/Rolls.js"
-
+import Dice1 from "./assets/1-dice.png"
+import Dice2 from "./assets/2-dice.png"
+import Dice3 from "./assets/3-dice.png"
+import Dice4 from "./assets/4-dice.png"
+import Dice5 from "./assets/5-dice.png"
+import Dice6 from "./assets/6-dice.png"
 
 const App = () => {
 
   const [startRoll, setCurrentRoll] = useState (0)
-  const [clearButton, setResetButton] = useState ([0])
+  const [clearButton, setResetButton] = useState ([])
+
 
   const rollDice = () => {
-    let diceGen = Math.floor(Math.random() * 7)
-    setCurrentRoll(diceGen)
-    setResetButton((prevRoll)=> {
-      const newRoll = prevRoll.slice()
-      newRoll.push(diceGen)
-      return newRoll
-    })
+    let diceGen = Math.floor(Math.random() *  6)+1
+    setCurrentRoll(diceGen);
+    setResetButton((prevRoll) => [...prevRoll, diceGen]);
   }
 
   const resetDice = () => {
-    setResetButton([0])
+    setResetButton([])
   }
   return (
 
     <>
     <div className="App"> 
     <h1> dice </h1> 
+   
       < Dice rollDice={rollDice} startRoll={startRoll}/>
       < Rolls clearButton={clearButton} resetDice={resetDice}/>
     </div>
--- props-adrian-louis/src/components/Rolls.js
+++ props-adrian-louis/src/components/Rolls.js
@@ -1,8 +1,16 @@
 import React from "react"
-const Rolls = () => {
+const Rolls = ({clearButton, resetDice}) => {
     return (
         <>
-        
+        <div className="Rolls">
+        <button onClick={resetDice}>Clear</button> 
+        <ul>
+            {clearButton.map((roll, index) => (
+                <li key={index}>{roll}</li>
+            ))}
+        </ul>
+        <h1>Roll Counter</h1>    
+        </div> 
         </>
     )
 
