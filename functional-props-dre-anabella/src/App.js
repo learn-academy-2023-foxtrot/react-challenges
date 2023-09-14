ecdfd609949478387d6a060822509092b7f5a9b5
alopezdelgado <andrealdelgado_99@outlook.com>
2023-09-14 14:00:18 -0700

    Made a map function to show the list of menu items, and made it pretty

--- functional-props-dre-anabella/src/App.js
+++ functional-props-dre-anabella/src/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-
+import './App.css'
 
 const App = () => {
   const [menu, setMenu] = useState([
@@ -10,7 +10,18 @@ const App = () => {
   ])
 
   return (
-    <></>
+    <>
+    <div className="background">
+      <h1 className="header">Dranabella's Gourmet Sushi</h1>
+      <ul>
+        <h2 className= "menu">{menu.map((item, index) => {
+          return (
+            <li key={index}>{item.entree} -- amount: $ {item.amount}</li>
+          )
+        })}</h2>
+      </ul>
+    </div>
+    </>
 
 
     );
@@ -29,5 +40,7 @@ export default App;
 // -we need to use props to share useState with the component menu
 // -we then need to use functional props to return .map to App.js
 
-Psuedo:
-input
+// Pseudo:
+// input: object 
+// output: unordered list of the menu items
+// process: use .map to iterate through the array to display our menu items, use dot notation to access the values within the object and return the unordered list. 
