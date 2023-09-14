cc7ea753058e67b6e744e04dd20279910252de13
aesandino <anabellasandino@gmail.com>
2023-09-14 15:32:53 -0700

    about halfway done with the second user story

--- functional-props-dre-anabella/src/App.js
+++ functional-props-dre-anabella/src/App.js
@@ -1,5 +1,7 @@
 import React, {useState} from "react";
 import './App.css'
+import Menu from './components/Menu.js'
+import Order from './components/Order.js'
 
 const App = () => {
   const [menu, setMenu] = useState([
@@ -8,18 +10,16 @@ const App = () => {
     {entree: "volcano roll", amount: 70},
     {entree: "seaweed salad", amount: 50}
   ])
-
+    // const addToOrder = (index) => {
+    //   menu[index].entree
+    //   setMenu([...menu])
+    // }
   return (
     <>
     <div className="background">
       <h1 className="header">Dranabella's Gourmet Sushi</h1>
-      <ul>
-        <h2 className= "menu">{menu.map((item, index) => {
-          return (
-            <li key={index}>{item.entree} -- amount: $ {item.amount}</li>
-          )
-        })}</h2>
-      </ul>
+      <Menu menu={menu} /> 
+      <Order /> 
     </div>
     </>
 
@@ -44,3 +44,16 @@ export default App;
 // input: object 
 // output: unordered list of the menu items
 // process: use .map to iterate through the array to display our menu items, use dot notation to access the values within the object and return the unordered list. 
+
+// Pseudo:
+// input: 
+// output: 4 buttons
+// process: 
+// -we want to be able to click on one of 4 buttons to see each item
+// -we want to place our button withiin the map function so that it iterates and creates a button for each menu item
+// -we need to import the useState from App.js to menu.js
+// -moving the .map function, using functional props to App.js
+
+// PLAN:
+// FOR ORDER COMPONENT:
+// -we want to have the entree orders listed within the order box after they've been clicked on. 
---
+++ functional-props-dre-anabella/src/components/Order.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Order = () => {
+    <>
+    <div className="order">
+
+    </div>
+    </>
+}
+export default Order
